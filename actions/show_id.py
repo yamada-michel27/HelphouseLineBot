6e9514a195219937752db4d65dc5f63e22681bf7
Taka0007 <7takacode@gmail.com>
2025-05-04 14:48:53 +0900

    idコマンド修正の取り込み

--- actions/show_id.py
+++ actions/show_id.py
@@ -8,10 +8,10 @@ def match(event: MessageEvent, message: str) -> bool:
 def action(event: MessageEvent, api_client: ApiClient, message: str):
     reply = "こんにちは、あなたのIDは" + event.source.user_id + "です！"
 
-    if event.source.group_id:
+    if hasattr(event.source, "group_id") and event.source.group_id:
         reply += "\nグループIDは" + event.source.group_id + "です！"
 
-    if event.source.room_id:
+    if hasattr(event.source, "group_id") and event.source.room_id:
         reply += "\nルームIDは" + event.source.room_id + "です！"
 
     return reply
